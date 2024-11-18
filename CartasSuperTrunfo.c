9f8b90a7fe2ae3daee5070186f9c8bb9fe93181f
9fbass <9fbass@gmail.com>
2024-11-18 00:52:41 +0000

    Modificações dia 17.11.24

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,35 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char estado;
+    int cidade;
+    int populacao;
+    int area;
+    int pib;
+    int turismo;
+
+printf("Digite a inicial do estado: \n");
+scanf("%c", &estado);
+
+printf("Digite o número da cidade: \n");
+scanf("%d", &cidade);
+
+printf("Digite o número da população: \n");
+scanf("%d", &populacao);
+
+printf("Digite o tamanho da área: \n");
+scanf("%d", &area);
+
+printf("Digite o número do PIB: \n");
+scanf("%d", &pib);
+
+printf("Digite o número de pontos turisticos: \n");
+scanf("%d", &turismo);
+
+
+printf("inicial do estado: %c - número da cidade: %d\n", estado, cidade);
+printf("número da população: %d - tamanho da área: %d\n", populacao, area);
+printf("número de PIB: %d - número de pontos turisticos: %d\n", pib, turismo);
 
 
     return 0;
